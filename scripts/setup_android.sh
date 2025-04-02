33282135fbd81438b6cde65e6eec40bc34ad7e1f
Jinyu <cadenho@hotmail.com>
2025-04-02 19:18:35 +0800

    fix local aar

--- scripts/setup_android.sh
+++ scripts/setup_android.sh
@@ -3,4 +3,4 @@
 # Android AAR 下载
 ANDROID_URL="https://github.com/hellohejinyu/ffmpeg_kit_flutter_full_gpl/releases/download/v5.1.LTS/ffmpeg-kit-full-gpl-5.1.LTS.aar"
 mkdir -p libs
-curl -L $ANDROID_URL -o libs/ffmpeg-kit-full-gpl-5.1.LTS.aar
+curl -L $ANDROID_URL -o libs/com.arthenica.ffmpegkit-flutter-5.1.aar
